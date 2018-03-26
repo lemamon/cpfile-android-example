2cab198b659d2937863707be35ea9d761a69e41e
Deyvid Marinho <dem@certi.org.br>
2018-03-26 17:53:21 -0400

    changes

--- app/src/main/java/com/example/dem/cpfiles/MainActivity.java
+++ app/src/main/java/com/example/dem/cpfiles/MainActivity.java
@@ -7,8 +7,6 @@ import android.content.UriPermission;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.provider.DocumentFile;
 import android.support.v7.app.AppCompatActivity;
@@ -17,15 +15,12 @@ import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
-
-import org.apache.commons.io.FileUtils;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     private Context ctx;
     private Uri source;
     private Uri target;
-    private String fileName;
 
     private final int REQUEST_CODE_SOURCE = 42;
     private final int REQUEST_CODE_TARGET = 43;
@@ -46,35 +40,39 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         ActivityCompat.requestPermissions(this,
                 new String[]{
                         Manifest.permission.READ_EXTERNAL_STORAGE,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 }, 44);
+
         ctx = this;
 
-        Button btFlash = findViewById(R.id.bt_flash);
-        btFlash.setOnClickListener(new View.OnClickListener() {
+        Button btnCopy = findViewById(R.id.btn_copy);
+        btnCopy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                flash();
+                boolean okCopy = copy(
+                    new File(FileUtil.getPath(ctx, source)),
+                    FileUtil.getFullPathFromTreeUri(target, ctx),
+                    ctx
+                );
+                setText(R.id.txt_copy, okCopy ? "Copy Ok" : "Copy NotOk");
             }
         });
 
-        Button btTarget = findViewById(R.id.bt_target);
-        btTarget.setOnClickListener(new View.OnClickListener() {
+        Button btnTarget = findViewById(R.id.btn_target);
+        btnTarget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.e("ttt", Environment.getExternalStorageDirectory().getAbsolutePath());
                 Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
                 intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
                 startActivityForResult(intent, REQUEST_CODE_TARGET);
             }
         });
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button btnSource = findViewById(R.id.btn_source);
+        btnSource.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -87,86 +85,43 @@ public class MainActivity extends AppCompatActivity {
                 openIntent.addCategory(Intent.CATEGORY_OPENABLE);
                 openIntent.setType("*/*");
                 startActivityForResult(openIntent, REQUEST_CODE_SOURCE);
-
-//
             }
         });
     }
 
-    public void flash() {
-        Log.e("flash", "sim");
-        File sourceLocation = new File(FileUtil.getPath(this, source));
-        File targetLocation = new File(FileUtil.getFullPathFromTreeUri(target, this) + "/" + fileName);
-
-
-        try
-        {
-            FileUtils.copyFile(sourceLocation, targetLocation);
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-//        try{
-//            Log.e("try", "sim");
-//            Log.v("try", "sourceLocation: " + sourceLocation);
-//            Log.v("try", "targetLocation: " + targetLocation);
-//            if(true){
-//                InputStream in  = new FileInputStream(sourceLocation);
-//                OutputStream out = new FileOutputStream(targetLocation);
-//                Log.e("existes", "sim");
-//
-//                byte[] buf = new byte[1024];
-//                int len;
-//                while ((len = in.read(buf)) > 0){
-//                    out.write(buf, 0, len);
-//                }
-//                in.close();
-//                out.close();
-//            }
-//
-//        }catch (NullPointerException e){
-//            e.printStackTrace();
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-    }
 
     public  void onActivityResult(int requestCode, int resultCode, Intent data) {
-        System.out.println(data);
+
+        if(data == null) return;
+
         switch (requestCode){
             case REQUEST_CODE_SOURCE:
                 source = data.getData();
-                Log.e("source", source+"");
-                fileName = getFileName(source.getPath());
-                Log.e("target", FileUtil.getPath(this, source));
-                Log.e("Name", fileName);
-                final int takeFlagss = data.getFlags()
-                        & (Intent.FLAG_GRANT_READ_URI_PERMISSION
-                        | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                getContentResolver().takePersistableUriPermission(source, takeFlagss);
+                takeFlags(data, source);
+                setText(R.id.txt_source, FileUtil.getPath(ctx, source));
                 break;
             case REQUEST_CODE_TARGET:
                 target = data.getData();
-                Log.e("target", FileUtil.getFullPathFromTreeUri(target, this));
-                final int takeFlagst = data.getFlags()
-                        & (Intent.FLAG_GRANT_READ_URI_PERMISSION
-                        | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                getContentResolver().takePersistableUriPermission(target, takeFlagst);
-                Log.e("target", target+"");
+                takeFlags(data, target);
+                setText(R.id.txt_target, FileUtil.getFullPathFromTreeUri(target, ctx));
+                break;
+            default:
+                System.out.println("No Option");
                 break;
         }
-        Log.e("requetecode", requestCode+"");
-        Log.e("resultCode", resultCode+"");
-
     }
 
-    private String getFileName (String srcPath) {
-        int idx = srcPath.lastIndexOf("/");
-        String name = srcPath.substring(idx + 1);
-        return name;
+    private  void takeFlags(Intent data, Uri dir) {
+        final int takeFlags = data.getFlags()
+                & (Intent.FLAG_GRANT_READ_URI_PERMISSION
+                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        getContentResolver().takePersistableUriPermission(dir, takeFlags);
     }
 
+    private void setText(int id, String txt ) {
+        TextView textView = findViewById(id);
+        textView.setText(txt);
+    }
 
     public static boolean copy(File copy, String directory, Context con) {
         FileInputStream inStream = null;
@@ -182,42 +137,29 @@ public class MainActivity extends AppCompatActivity {
             int bytesRead;
             while ((bytesRead = inStream.read(buffer)) != -1) {
                 outStream.write(buffer, 0, bytesRead);
-
             }
-
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-
                 inStream.close();
-
                 outStream.close();
-
-
                 return true;
-
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
         }
-
         return false;
     }
-
 
     public static DocumentFile getDocumentFileIfAllowedToWrite(File file, Context con) {
 
         List<UriPermission> permissionUris = con.getContentResolver().getPersistedUriPermissions();
 
         for (UriPermission permissionUri : permissionUris) {
-
+            Log.i("URI Permission", permissionUri.getUri().toString());
             Uri treeUri = permissionUri.getUri();
             DocumentFile rootDocFile = DocumentFile.fromTreeUri(con, treeUri);
             String rootDocFilePath = FileUtil.getFullPathFromTreeUri(treeUri, con);
@@ -248,21 +190,14 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     return null;
                 }
-
             }
         }
         return null;
     }
 
     public static String mime(String URI) {
-        String type;
         String ext = MimeTypeMap.getFileExtensionFromUrl(URI);
-        if (ext != null) {
-            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(ext);
-        }else {
-            type = "";
-        }
-        return type;
+        return ext != null ? MimeTypeMap.getSingleton().getMimeTypeFromExtension(ext) : "";
     }
 
 }
